830d60cf157602503cae61e466cbae111003c615
Huan Ning <gladcolor@gmail.com>
2021-02-18 12:47:26 -0500

    Fixbug: GSV_pano:refactorJson() where get ['Locatoin'['region'].

--- gsv_pano/utils.py
+++ gsv_pano/utils.py
@@ -61,14 +61,21 @@ def refactorJson(jdata):
         newJson['Location']['elevation_wgs84_m'] = ""
         newJson['Location']['description'] = jdata[1][0][3][2][0][0]
         newJson['Location']['streetRange'] = ''
-        try:
-            newJson['Location']['country'] = jdata[1][0][5][0][1][4]
-        except Exception as e:
-            print("Error in obtain newJson['Location']['country']:", e)
-        try:
-            newJson['Location']['region'] =  jdata[1][0][3][2][1][0]
-        except Exception as e:
-            print("Error in obtain newJson['Location']['region']:", e)
+
+        newJson['Location']['country'] = ''
+        newJson['Location']['region'] = ''
+
+        if len(jdata[1][0][5][0][1]) > 4:
+            try:
+                newJson['Location']['country'] = jdata[1][0][5][0][1][4]
+            except Exception as e:
+                print("Error in obtain new Json['Location']['country']:", e)
+
+        if len(jdata[1][0][3][2][1]) > 0:
+            try:
+                newJson['Location']['region'] =  jdata[1][0][3][2][1][0]
+            except Exception as e:
+                print("Error in obtain new Json['Location']['region']:", e)
 
         newJson['Location']['elevation_egm96_m'] = jdata[1][0][5][0][1][1][0]
 
